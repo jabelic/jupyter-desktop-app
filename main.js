3da69ab461c36378c0c2146d1fc8085b98ef718d
Jabelic <hibiki990523@gmail.com>
2021-01-27 03:58:33 +0900

    XXX: launch Jupyter lab depends on pipenv

--- main.js
+++ main.js
@@ -3,50 +3,146 @@ const fs = require('fs')
 const { spawn } = require('child_process');
 const ls = spawn('ls', ['-lh', '/usr']);
 const path = require('path')
-const processes = []
+let win = null
+// let processes = []
+let ready = false
+let openUrlFilePath= ""
+
+let windows = {}
+
+const findWindow = (filePath) => {
+    const rootLacation = findRootLocation(filePath)
+    return windows[rootLacation]
+}
+const findRootLocation = (filePath) => {
+    if (filePath == '/'){
+        return null
+    }
+    const dir = path.dirname(filePath)
+    const files = fs.readdirSync(dir) // ファイル一覧が取れる
+    const matches = files.filter((f) => {
+        return [".git", "requirements.txt", ".ipynb_checkpoint"].includes(f)
+    })
+    if (matches.length){
+        return dir
+    }
+    return findRootLocation(dir) //上のディレクトリを調べる. 再帰.
+}
+
 const startLab = (appPath) => {
-    // 1. .git, requirements.txt, .ipynb_checkpoints
-    // 2. file directory
-    const dir = path.dirname(appPath) // jupyterのところまで取れる
-    const cp = spawn("jupyter", ["lab"], {
-        cwd: dir,
+    const w = findWindow(appPath)
+    if(w){
+        openFile(w, appPath)
+        return
+    }
+    const rootLacation = findRootLocation(appPath) ||  path.dirname(appPath) // jupyterのところまで取れる
+    
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+        // nodeIntegration: true
+        }
     })
-    processes.push(cp)
-    cp.stdout.on('data', (data)=>{
-        console.log(data)
+
+    // const pyenv = fs.existsSync(path.join(process.env.HOME, ".pyenv"))
+    // console.log(":::",process.env.HOME)
+    // const command = pyenv ? path.join(process.env.HOME, ".pyenv", ".shims", "pipenv") : "pipenv"
+    const command = path.join("/usr/local/bin/", "pipenv")
+    // const command = path.join("/usr/local/Cellar/pipenv/2020.11.15/libexec/bin/", "pipenv")
+    const pipfile = spawn(command, ["pipenv", "install", "JupyterLab"])
+    const cp = spawn(command, ["run", "jupyter", "lab", "--no-browser"], {
+        cwd: rootLacation,
     })
+
+    const newWindow = {
+        root: rootLacation,
+        window,
+        process: cp,
+    }
+    windows[rootLacation] = newWindow
+    window.on('closed', () => {
+        cp.kill()
+        windows[rootLocation] = null
+    })
+
+    let outData = ""
+    let loaded = false
+    const dataListerner = (data)=>{
+        if (loaded) return 
+        outData += data.toString()
+        const match = outData.match(/http:\/\/.*/)
+        if(match){
+            loaded = true
+            console.log(match[0])
+            window.loadURL(match[0])
+            cp.stderr.off("data", dataListerner)
+            const fp = appPath.substring(rootLacation.length) // 
+            let url = `${match[0].split("?")[0]}/tree${fp}` // for jupyter lab.
+            console.debug(url)
+            setTimeout(()=>{
+                window.loadURL(url)
+            }, 1000) // Token認証を待つ
+        }
+        console.log("*****************************")
+        // console.log(data.toString())
+    } 
+    cp.stderr.on('data', dataListerner)
+}
+const openFile = (window, filePath) => {
+    const url = createUrl(window.url, window.root, filePath)
+    window.window.loadURL(url)
+    window.window.focus()
 }
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+        // nodeIntegration: true
+        }
+    })
 
-  // win.loadFile('index.html')
-  const appPath = '/Users/jabelic/dev/elec/jupyter/Untitled.ipynb'
-  startLab(appPath)
-//   win.loadURL('http://localhost:8888/?token=72bdeac4a38f9ca2508d495363a8b84bf9840cae3d31ef92')
-//   const root = fs.readdirSync('.')
-//   console.log(root)
+    win.loadFile('index.html')
+    
+    // const appPath = '/Users/jabelic/dev/elec/jupyter/Untitled.ipynb'
+    // startLab(appPath)
+    
+    // win.loadURL('http://localhost:8888/?token=72bdeac4a38f9ca2508d495363a8b84bf9840cae3d31ef92')
+    // const root = fs.readdir Sync('.')
+    // console.log(root)
 
-  win.on('closed', ()=>{
-      win = null
-  })
+    win.on('closed', ()=>{
+        win = null
+    })
 }
 
-app.whenReady().then(createWindow)
-app.on('open-file', (event, filePath) => {
-    console.log(filePath)
-    // event.preventDefault();
-    if (myApp.mainWindow) {
-        startLab(appPath)
-    //   myApp.mainWindow.webContents.send('dropTextFile', filePath);
+// app.whenReady().then(()=>{
+//     ready = true
+//     if (openUrlFilePath){
+//         startLab(openUrlFilePath)
+//         openUrlFilePath = ""
+//     }else{
+//         createWindow()
+//     }
+// })
+app.on('ready', ()=>{
+    ready = true
+    if (openUrlFilePath){
+        startLab(openUrlFilePath)
+        openUrlFilePath = ""
+    }else{
+        createWindow()
+    }
+})
+
+
+app.on('open-file', (_, filePath) => {
+    if (!ready) {
+        openUrlFilePath = filePath
     } else {
-    //   launchArgs = { filePath: filePath };
+        startLab(filePath)
     }
 })
 
